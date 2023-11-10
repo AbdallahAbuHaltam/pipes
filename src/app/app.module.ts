e0df808ed46ddac7854ccc098fbf093a3330908d
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-10 16:45:12 +0300

    Created Custom Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ShortenPipe } from './shorten.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
